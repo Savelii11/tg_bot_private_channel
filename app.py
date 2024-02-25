ed87ebaae07b835bdefc015ca4b56f2a73b9ab7a
turnMeUpSon <milleryan2003@gmail.com>
2024-02-25 23:13:22 +0000

    Added API url for POST request to create telegram user and send data to API microservice for handling subscriptions

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import handlers
 from aiogram import types
 from aiogram.types import ReplyKeyboardMarkup, ReplyKeyboardRemove
 from data import config
-from loader import dp, db, bot
+from loader import dp, bot
 import filters
 import logging
 from loader import dp
@@ -12,35 +12,35 @@ import sys
 
 
 
-WEBAPP_HOST = "0.0.0.0"
+'''WEBAPP_HOST = "0.0.0.0"
 WEBAPP_PORT = int(os.environ.get("PORT", 5000))
 user_message = 'Пользователь'
-admin_message = 'Админ'
+admin_message = 'Админ'''
 
 
 
 
-async def on_startup(dp):
+'''async def on_startup(dp):
     logging.basicConfig(level=logging.INFO)
     db.create_tables()
 
     await bot.delete_webhook()
-    await bot.set_webhook(config.WEBHOOK_URL)
+    await bot.set_webhook(config.WEBHOOK_URL)'''
 
 
-async def on_shutdown():
+'''async def on_shutdown():
     logging.warning("Shutting down..")
     await bot.delete_webhook()
     await dp.storage.close()
     await dp.storage.wait_closed()
-    logging.warning("Bot down")
+    logging.warning("Bot down")'''
 
 
 async def main() -> None:
     # Initialize Bot instance with a default parse mode which will be passed to all API calls
 
     # And the run events dispatching
-    await dp.start_polling(bot, on_startup=on_startup, skip_updates=False)
+    await dp.start_polling(bot, skip_updates=False)
 
 
 if __name__ == "__main__":
--- handlers/user/menu.py
+++ handlers/user/menu.py
@@ -5,6 +5,8 @@ from aiogram.filters import Command
 from keyboards.inline.products_from_cart import price_markup
 from states import Dialogue_state
 from aiogram import types, F
+import requests
+from data.config import SUBSCRIPTONS_SERVICE_API_URL
 
 
 '''@dp.message_handler(IsAdmin(), commands='menu')
@@ -18,6 +20,25 @@ async def start_dialogue(message: Message, state: FSMContext):
     #await Dialogue_state.view_subscription.set()
     await message.answer("Привет, этот бот дает возможность оплатить доступ в клуб «Баффеты на Уораннах»Наш клуб это не просто место, где вы сможете получить инструменты и идеи, для увеличения своего заработка. Это сообщества, где вы всегда получите обратную связь, поддержку, общение с единомышленниками. Что Вам даст клуб? - Прибыльный долгосрочный портфель- Заработок на торговых сетапахАналитику рынкаЗакрытые материалыОбщениеЕженедельные дайджесты",
                          reply_markup=price_markup())
+    
+    # Data to send in the POST request
+    data = {
+        'telegram_id': message.from_user.id,
+        'chat_id': message.chat.id,
+        'telegram_username': message.from_user.username,
+        'first_name': message.from_user.first_name,
+        'last_name': message.from_user.last_name,
+    }
+
+    # Sending the POST request
+    response = requests.post(url=SUBSCRIPTONS_SERVICE_API_URL, data=data)
+
+    # Checking the response status code
+    if response.status_code == 201:
+        await message.answer("Вы успешно зарегестрированы!")
+    else:
+        await message.answer("Вы уже в БД")
+    
     await state.set_state(Dialogue_state.view_subscription)
 
 @router.callback_query(F.data == 'two_days')
